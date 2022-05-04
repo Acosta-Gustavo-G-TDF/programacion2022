de32e5403f943d921b69afdbcf158b250bdd8e06
Acosta-Gustavo-G-TDF <104173849+Acosta-Gustavo-G-TDF@users.noreply.github.com>
2022-05-04 18:24:53 -0300

    Programacion2022

--- src/AumentoElseIf.ts
+++ src/AumentoElseIf.ts
@@ -3,23 +3,23 @@ let aumento: number;
 
 if (sueldo > 0 && sueldo <= 15000) {
   aumento = sueldo * 1.2;
-  console.log("El nuevo sueldo del empleado es:", aumento - sueldo);
-} else if ((sueldo) => 15001 && sueldo <= 20000) {
+  console.log("El aumento de sueldo del empleado es:", aumento - sueldo);
+} else if (sueldo > 15000 && sueldo <= 20000) {
   aumento = sueldo * 1.1;
-  console.log("El nuevo sueldo del empleado es:", aumento - sueldo);
-} else if ((sueldo) => 20001 && sueldo <= 25000) {
+  console.log("El aumento de sueldo del empleado es:", aumento - sueldo);
+} else if (sueldo > 20000 && sueldo <= 25000) {
   aumento = sueldo * 1.05;
-  console.log("El nuevo sueldo del empleado es:", aumento - sueldo);
+  console.log("El aumento de sueldo del empleado es:", aumento - sueldo);
 } else {
   console.log("No hay aumento");
 }
 /*
 Datos de entrada                          Salida deseada 
 
-sueldo > 0 && sueldo < 15000              sueldo * 1.2
+sueldo > 0 && sueldo <= 15000             Aumento 20%
 
-sueldo > 15001 && sueldo < 20000          sueldo * 1.1
+sueldo => 15001 && sueldo <= 20000        Aumento 15%
 
-sueldo > 20001 && sueldo < 25000          sueldo * 1.05
+sueldo => 20001 && sueldo <= 25000        Aumento 10%
 
-sueldo > 25000                            No hay aumento de sueldo*/
+sueldo > 25000                            No hay aumento*/
