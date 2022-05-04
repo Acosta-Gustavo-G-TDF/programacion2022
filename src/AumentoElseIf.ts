412310f23a5e6713b15449cbc25514234a61a7dd
Acosta-Gustavo-G-TDF <104173849+Acosta-Gustavo-G-TDF@users.noreply.github.com>
2022-05-04 18:08:07 -0300

    Programacion2022

--- src/AumentoElseIf.ts
+++ src/AumentoElseIf.ts
@@ -1,15 +1,15 @@
 let sueldo: number = Number(prompt("Ingrese el sueldo del empleado:"));
-let nuevoSueldo: number;
+let aumento: number;
 
-if (sueldo > 0 && sueldo < 15000) {
-  nuevoSueldo = sueldo * 1.2;
-  console.log("El nuevo sueldo del empleado es:", nuevoSueldo);
-} else if (sueldo > 15001 && sueldo < 20000) {
-  nuevoSueldo = sueldo * 1.1;
-  console.log("El nuevo sueldo del empleado es:", nuevoSueldo);
-} else if (sueldo > 20001 && sueldo < 25000) {
-  nuevoSueldo = sueldo * 1.05;
-  console.log("El nuevo sueldo del empleado es:", nuevoSueldo);
+if (sueldo > 0 && sueldo <= 15000) {
+  aumento = sueldo * 1.2;
+  console.log("El nuevo sueldo del empleado es:", aumento - sueldo);
+} else if ((sueldo) => 15001 && sueldo <= 20000) {
+  aumento = sueldo * 1.1;
+  console.log("El nuevo sueldo del empleado es:", aumento - sueldo);
+} else if ((sueldo) => 20001 && sueldo <= 25000) {
+  aumento = sueldo * 1.05;
+  console.log("El nuevo sueldo del empleado es:", aumento - sueldo);
 } else {
   console.log("No hay aumento");
 }
